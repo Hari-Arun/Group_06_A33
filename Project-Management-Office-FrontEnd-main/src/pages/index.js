5ee42101d52b273aee44dc94c569c5167009a0bf
Hari-Arun <127079982+Hari-Arun@users.noreply.github.com>
2023-04-10 21:01:48 +0530

    Delete Project-Management-Office-FrontEnd-main directory

--- Project-Management-Office-FrontEnd-main/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export {default as Employee} from './Employee'
-export {default as Home} from './Home'
-export {default as CompanyHome} from './CompanyHome'
-export {default as Customer} from './Customer'
-export {default as Scrum} from './Scrum'
-export {default as Team} from './Team'
-export {default as ScrumDetail} from './ScrumDetail'
